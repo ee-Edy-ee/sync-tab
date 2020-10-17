876678e7fe924ee3d89080ed289b076d8041801c
Edy <eduardcosmin14@gmail.com>
2020-10-18 02:51:16 +0300

    add ripple directive

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,12 @@
         <div class="hello">
             <h1>{{ msg }}</h1>
 
-            <a href="#" class="btn btn-primary" v-ripple="{ class: 'text-danger' }">Test</a>
+            <a href="javascript:void(0)" class="btn btn-primary" v-ripple>Test</a>
+            <a href="javascript:void(0)" class="btn btn-warning" v-ripple>Test</a>
+            <a href="javascript:void(0)" class="btn btn-success" v-ripple>Test</a>
+            <a href="javascript:void(0)" class="btn btn-info" v-ripple>Test</a>
+
+            <div class="bg-dark" v-ripple="{ class: 'text-light' }" style="height: 50px; width: 200px"></div>
         </div>
     </div>
 </template>
