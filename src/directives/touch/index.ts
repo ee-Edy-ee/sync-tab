3dc2c990128eb7518b6d799ac92cffeb6234584c
Edy <eduardcosmin14@gmail.com>
2020-10-18 02:36:03 +0300

    add ripple directive

---
+++ src/directives/touch/index.ts
@@ -0,0 +1,5 @@
+export interface TouchStoredHandlers {
+    touchstart: (e: TouchEvent) => void;
+    touchend: (e: TouchEvent) => void;
+    touchmove: (e: TouchEvent) => void;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import App from "@/App.vue";
+import "@/registerServiceWorker";
+import router from "@/router";
+import store from "@/store";
+import Ripple from "@/directives/ripple";
 
-createApp(App)
-    .use(store)
+const app = createApp(App);
+app.directive(Ripple.name, Ripple.directive);
+
+app.use(store)
     .use(router)
     .mount("#app");
+
+console.log(process.env.NODE_ENV);
